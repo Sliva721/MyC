3c8dc9008762c741febc4f38a7fa1c51cf777def
Sliva721 <slivin721@gmail.com>
2024-08-14 21:58:38 +0300

    Add variable_fabs.c

---
+++ variable_fabs.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <math.h>
+int main() {
+ int x1,x2,res; //x1= -39; X2=62
+   scanf("%d %d",&x1,&x2); 
+   res=fabs(x2-x1); 
+   printf("%d\n", res); //verniy rezultat = 101
+    return 0;
+}
