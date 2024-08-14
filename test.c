51c5c4f3eaa0a20aa28ff40a3f1d6d429e74f4e4
Sliva721 <slivin721@gmail.com>
2024-08-14 16:28:30 +0300

    Delete test and modified test.c

--- test.c
+++ test.c
@@ -3,12 +3,11 @@
 int main (void) {
   int a, b, c2;
 
-  printf("Vvesti dlinnu 1 kateta = \n");
   scanf("%d",&a);
-  printf("Vvesti dlinnu 2 kateta = \n");
   scanf("%d",&b);
-	c2 =  a*a + b*b;
- printf("c = %.2f\n", sqrt(c2)); 
+	
+  c2 =  a*a + b*b;
+  printf("c = %.2f\n", sqrt(c2)); 
 
   return 0;
 }
