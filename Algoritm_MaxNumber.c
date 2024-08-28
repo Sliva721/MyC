a89160c3edf1fdb3959800fff1519a21bb5de1e5
Sliva721 <slivin721@gmail.com>
2024-08-28 15:36:25 +0300

    Add new files

---
+++ Algoritm_MaxNumber.c
@@ -0,0 +1,23 @@
+//стандартный алгоритм поиска максимального числа.
+
+#include <stdio.h>
+
+int main(void) {
+
+  int N = 0;
+
+  scanf("%d", &N);
+
+  int max = -9999;
+  int number = 0;
+
+  for (int i = 1; i <= N; i++){
+    scanf("%d", &number);
+    if (max < number)
+      max = number;
+  }
+
+  printf("%d\n", max);
+
+  return 0;
+}
