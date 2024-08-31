e07d69b1eb4b82d85ad15ab0daf2bca0247ceacf
Sliva721 <slivin721@gmail.com>
2024-08-31 21:13:55 +0300

    Add new files

---
+++ Cycle For While.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int N;
+      scanf("%d",&N);
+    do  {
+       if (N < 0 || N == -9999) {
+          printf("NO\n");
+           printf("%d\n",N);
+           break;
+                 }
+                   scanf("%d",&N);
+               }
+       while (N !=-9999);
+printf ("YES");    
+
+       return 0;
+}
