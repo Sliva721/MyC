61ed033ed2f7736abe606a7d4a06e5f03aac4e5c
Sliva721 <slivin721@gmail.com>
2024-08-31 12:16:13 +0300

    Add 2 new Files Factorial and Exponenta

---
+++ Exponenta and Faktorial.c
@@ -0,0 +1,18 @@
+//                             Экспонента с заданной точностью
+//   Вычислить число e с точностью ε
+
+
+
+#include <stdio.h>
+
+int main() {
+    double e, s=1, f=1, i=1; 
+    scanf("%lf", &e);
+    while((1/f)>e){
+        f = f*i;
+        s = s+1/f;
+        i++;
+    }
+  printf("%.8lf", s);
+  return 0;
+}
