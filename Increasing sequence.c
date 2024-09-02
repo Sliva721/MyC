fb7cc4fe0179c1d050afff77c28dfe32c5b61050
Sliva721 <slivin721@gmail.com>
2024-09-02 22:26:41 +0300

    Add new files

---
+++ Increasing sequence.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int N,p=-9998;
+    scanf("%d",&N);
+     while (N !=-9999) {
+  //printf ("N=%d p=%d\n", N,p); //проверяем какие значения будут для сравнения цикле
+       if (p >= N && p>0 ) {
+          printf("NO\n");
+                return 0; }
+                p=N;
+               scanf("%d",&N);
+                    
+               }
+       
+printf("YES");
+    return 0;
+}
