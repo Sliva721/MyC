2bfadb979b47cddfb6a1d3b70322e632f4d8227f
Sliva721 <slivin721@gmail.com>
2024-09-04 09:54:01 +0300

    Add new files Secret Code.c

---
+++ Secret Code.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+ int main() {
+   int N=0,k=0;
+   
+        while (k != 2517 ) 
+     {
+         scanf("%d",&N);
+         k=N;
+       //printf ("N=%d  k=%d\n", N,k); //проверяем какие значения будут  в цикле (гоним до k=2517)
+     }  
+  
+   do  {
+         scanf("%d",&N);
+     //  printf ("N=%d  k=%d\n", N,k); // проверяем значения
+       if (k==2517 && N==-9999) {
+           break;}
+         printf ("%d ", N);
+       }
+   
+   while (N != -9999);
+   
+ return 0;
+}
