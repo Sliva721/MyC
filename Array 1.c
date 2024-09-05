b1ec53c4811fe083284e60c83256d4f665ffd01b
Sliva721 <slivin721@gmail.com>
2024-09-05 17:35:01 +0300

    Add new file Array1

---
+++ Array 1.c
@@ -0,0 +1,18 @@
+///                    МАССИВЫ (вводное занятие)
+
+#include <stdio.h>
+
+int main()
+{
+
+    int N = 0;
+    scanf("%d", &N);
+    int arr[N];
+
+    for (int i = 0; i < N; ++i)
+        scanf("%d", arr + i);
+    for (int i = N - 1; i >= 0; --i)
+        printf("%d ", arr[i]);
+
+    return 0;
+}
