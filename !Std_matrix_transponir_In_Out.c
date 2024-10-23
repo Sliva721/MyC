be2a92c9ce0cb90e4fce25e9753b6c32d43d00a4
Sliva721 <slivin721@gmail.com>
2024-10-23 22:21:28 +0300

    Add files and update ignore

---
+++ !Std_matrix_transponir_In_Out.c
@@ -0,0 +1,24 @@
+// Транспонирование
+// Для заданной матрицы вывести на экран транспонированную матрицу.
+
+#include <stdio.h>
+
+int main() {
+    int n, m;
+    scanf("%d%d", &n, &m);
+
+    //
+    int arr[n][m];
+    for (int i = 0; i < n; i++)        
+        for (int j = 0; j < m; j++) {  //    СЧИТЫВАЕМ (scanf) ЗНАЧЕНИЯ ПОСТРОЧНО
+            scanf("%d", &arr[i][j]);
+        }  
+
+    for (int j = 0; j < m; j++) {       
+        for (int i = 0; i < n; i++)      //   ВЫВОДИМ НА ЭКРАН СТОЛБЦЫ но в СТРОКИ
+            printf("%d ", arr[i][j]);  //
+        printf("\n");
+    }
+
+    return 0;
+}
