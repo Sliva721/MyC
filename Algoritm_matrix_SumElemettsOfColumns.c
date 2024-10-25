bac3d19db60e883e0084d65f35fa0d168116b2f4
Sliva721 <slivin721@gmail.com>
2024-10-25 21:51:05 +0300

    Updade 3 files Algoritm

--- Algoritm_matrix_SumElemettsOfColumns.c
+++ Algoritm_matrix_SumElemettsOfColumns.c
@@ -1,36 +1,34 @@
-//                Столбцы
-// Для заданной матрицы посчитать сумму элементов каждого столбца.
-// Два натуральных числа  N и M.
-// Первой вывести сумму последнего столбца, второй предпоследнего и т.д.
-// Sample Input:
-//          3 4
-//          1 2 3 4
-//          1 3 4 5
-//          0 2 3 -2
+/*                Столбцы
+ Для заданной матрицы посчитать сумму элементов каждого столбца.
+ Два натуральных числа  N и M.
+ Поменять местами столбцы с максимальной и минимальной суммой.
+ Sample Input:
+3 4
+1 2 3 4
+1 3 4 5
+0 2 3 -2
+*/ 
 
 #include <stdio.h>
 
 int main() {
-    int N, M, a = 0;
-    scanf("%d%d", &N, &M);
-    //         printf ("%d %d\n",N,M);
-    int A[N][M];
-    int B[M];
-    int i, j;
-    for (i = 0; i < N; ++i)
-        for (j = 0; j < M; ++j) {
-            scanf("%d", &A[i][j]);
-        }
+    int n, m ;
+    scanf("%d %d", &n, &m);
+    int arr[n][m];
 
-    for (j = 0; j < M; ++j) {
-        for (i = 0; i < N; ++i) a += A[i][j];
-        //                printf("a=%d \n",a);
-        B[j] = a;
-        //                                 printf("B[j]=%d \n",B[j]);
+    for (int i = 0; i < n; i++){
+        for (int j = 0; j < m; j++)
+            scanf("%d", &arr[i][j]);
 
-        a = 0;
-
-        for (j = M - 1; j >= 0; j = j - 1) printf("%d ", B[j]);
-
-        return 0;
     }
+
+    for (int j = m-1; j >= 0; j--){
+        int column = 0;
+        for (int i = 0; i < n; i++)
+            column += arr[i][j];
+        printf("%d ", column);
+
+    }
+
+    return 0;
+}
---
+++ Algoritm_max_number.c
@@ -0,0 +1,21 @@
+//Cтандартный алгоритм поиска максимального числа.
+
+#include <stdio.h>
+
+int main(void) {
+    int n = 0;
+
+    scanf("%d", &n);
+
+    int max = -9999;
+    int number = 0;
+
+    for (int i = 1; i <= n; i++) {
+        scanf("%d", &number);
+        if (max < number) max = number;
+    }
+
+    printf("%d\n", max);
+
+    return 0;
+}
